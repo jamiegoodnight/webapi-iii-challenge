d02f1935b0ebb932fd4b8a2454bcf80a3e62a24f
Jamie Goodnight <goodnightlambda@gmail.com>
2019-03-21 15:27:07 -0400

    Fixed port. Should work?

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = require("./server.js");
 
 const port = process.env.PORT || 4000;
 const greeting = process.env.GREETING;
-server.listen(5000, () => {
+server.listen(port, () => {
   console.log(
     `\n*** ${greeting} Server Running on http://localhost:${port} ***\n`
   );
